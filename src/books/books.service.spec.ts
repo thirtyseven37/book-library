0882df80215bee87d02aa3afdcea293424a32aaa
Patryk Sienkiewicz <p.sienkiewicz@tme3c.com>
2020-03-16 08:15:03 +0100

    Unit tests fix

--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -1,18 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksService } from './books.service';
 
+class BookRepositoryMock {}
+
 describe('BooksService', () => {
-  // let service: BooksService;
-  //
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     providers: [BooksService],
-  //   }).compile();
-  //
-  //   service = module.get<BooksService>(BooksService);
-  // });
-  //
-  // it('should be defined', () => {
-  //   expect(service).toBeDefined();
-  // });
+  let service: BooksService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        BooksService,
+        {
+          provide: 'BookRepository',
+          useClass: BookRepositoryMock,
+        },
+      ],
+    }).compile();
+
+    service = module.get<BooksService>(BooksService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
 });
